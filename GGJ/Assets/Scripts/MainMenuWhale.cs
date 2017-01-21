7fc44a4a961f53a67672f1318fe73d46fbf1f694
Piotr Wiącek <pi.wiacek@gmail.com>
2017-01-21 20:25:43 +0100

    - Whale in main menu sway a little.

--- GGJ/Assets/Scripts/MainMenuWhale.cs
+++ GGJ/Assets/Scripts/MainMenuWhale.cs
@@ -6,22 +6,26 @@ public class MainMenuWhale : MonoBehaviour {
 
     [SerializeField]
     private Transform tail;
-    const float MAX_ROTATION = 25f;
+    const float MAX_TAIL_ROTATION = 25f;
+    const float MAX_BODY_ROTATION = 5f;
 
-	private void Start () {
-        tail.localEulerAngles = new Vector3(0f, 0f, -MAX_ROTATION);
-        tail.DORotate(new Vector3(0f, 0f, 2 * MAX_ROTATION), 2f, RotateMode.LocalAxisAdd)
+    private void Start () {
+        tail.localEulerAngles = new Vector3(0f, 0f, -MAX_TAIL_ROTATION);
+        tail.DORotate(new Vector3(0f, 0f, 2 * MAX_TAIL_ROTATION), 2f, RotateMode.LocalAxisAdd)
             .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine).SetId("WhaleTail" + GetInstanceID());
-	}
+        transform.DORotate(new Vector3(0f, 0f, 2 * MAX_BODY_ROTATION), 2f, RotateMode.LocalAxisAdd)
+            .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine).SetId("WhaleBody" + GetInstanceID());
+    }
 
     // Swim off the screen.
     public void SwimAway(bool right)
     {
         int sign = (right) ? 1 : -1; 
-        DOTween.Kill("WhaleTail" + GetInstanceID(), false);
+        DOTween.Kill("WhaleTail" + GetInstanceID());
+        DOTween.Kill("WhaleBody" + GetInstanceID());
         DOTween.To(() => tail.localEulerAngles,
             (Vector3 rotation) => tail.localEulerAngles = rotation,
-            new Vector3(0f, 0f, sign * MAX_ROTATION), 0.5f)
+            new Vector3(0f, 0f, sign * MAX_TAIL_ROTATION), 0.5f)
             .OnComplete(() => transform.parent.DORotate(new Vector3(0f, 0f, -sign * 160f),
                 2f, RotateMode.LocalAxisAdd));
     }
