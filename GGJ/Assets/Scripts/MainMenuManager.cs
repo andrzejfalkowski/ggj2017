8b37e368f3f80986e07934475a8501d03e000529
Piotr Wiącek <pi.wiacek@gmail.com>
2017-01-21 11:47:07 +0100

    - Main menu tv-like switch of upon starting the game.

--- GGJ/Assets/Scripts/MainMenuManager.cs
+++ GGJ/Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
 public class MainMenuManager : MonoBehaviour {
-	
-	void Update () {
+
+    [SerializeField]
+    private CanvasGroup texts;
+    [SerializeField]
+    private Transform background;
+    [SerializeField]
+    private Image flash;
+    const float FADE_OUT_TIME = 0.5f;
+
+    void Update () {
 	    if (Input.anyKeyDown)
         {
             StartGame();
@@ -12,6 +22,16 @@ public class MainMenuManager : MonoBehaviour {
 
     public void StartGame()
     {
-        Application.LoadLevel("Gameplay");
+        DOTween.To(() => texts.alpha, (float alpha) => texts.alpha = alpha, 0f, FADE_OUT_TIME)
+            .SetEase(Ease.OutCirc);
+        DOTween.To(() => background.localScale,
+            (Vector3 scale) => background.localScale = scale, new Vector3(4f, 0f), FADE_OUT_TIME)
+            .SetEase(Ease.OutCirc);
+        DOTween.ToAlpha(() => flash.color, (Color c) => flash.color = c, 1f, FADE_OUT_TIME);
+        DOTween.To(() => texts.transform.localScale,
+            (Vector3 scale) => texts.transform.localScale = scale, new Vector3(4f, 0f), FADE_OUT_TIME)
+            .SetEase(Ease.OutCirc);
+
+        DOVirtual.DelayedCall(2f, () => Application.LoadLevel("Gameplay"));
     }
 }
