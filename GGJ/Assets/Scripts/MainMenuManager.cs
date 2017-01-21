24980b7d791250c925abddd6536114c6f4874907
Piotr Wiącek <pi.wiacek@gmail.com>
2017-01-21 10:58:07 +0100

    - Main menu.

---
+++ GGJ/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+	
+	void Update () {
+	    if (Input.anyKeyDown)
+        {
+            StartGame();
+        }
+	}
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Gameplay");
+    }
+}
---
+++ GGJ/Assets/Scripts/MainMenuWhale.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class MainMenuWhale : MonoBehaviour {
+
+    [SerializeField]
+    private Transform tail;
+    const float MAX_ROTATION = 25f;
+
+	private void Start () {
+        tail.localEulerAngles = new Vector3(0f, 0f, -MAX_ROTATION);
+        //DOTween.To(tail.localEulerAngles,
+        //    (Vector3 rotation) => tail.localEulerAngles = rotation,
+        //    new Vector3(0f, 0f, -30f), 2f);
+        tail.DORotate(new Vector3(0f, 0f, 2 * MAX_ROTATION), 2f, RotateMode.LocalAxisAdd)
+            .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+	}
+}
